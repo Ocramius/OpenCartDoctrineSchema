046f0469b09aaccf9e6f55ca679a077365f4691e
Marco Pivetta <ocramius@gmail.com>
2015-09-13 16:06:39 +0100

    Schema tool bootstrapper

---
+++ cli-config.php
@@ -0,0 +1,34 @@
+<?php
+
+use Doctrine\DBAL\Driver\PDOMySql\Driver;
+use Doctrine\DBAL\Logging\EchoSQLLogger;
+use Doctrine\ORM\Configuration;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Mapping\Driver\XmlDriver;
+use Doctrine\ORM\Proxy\ProxyFactory;
+use Doctrine\ORM\Tools\Console\ConsoleRunner;
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$configuration = new Configuration();
+
+$configuration->setMetadataDriverImpl(new XmlDriver([__DIR__ . '/mappings']));
+$configuration->setProxyDir(sys_get_temp_dir() . '/example' . uniqid('', true));
+$configuration->setProxyNamespace('ProxyExample');
+$configuration->setAutoGenerateProxyClasses(ProxyFactory::AUTOGENERATE_EVAL);
+
+$entityManager = EntityManager::create(
+    [
+        'driverClass' => Driver::class,
+        'user'        => 'root',
+        'password'    => null,
+        'dbname'      => 'opencart',
+    ],
+    $configuration
+);
+
+$configuration->setSQLLogger(new EchoSQLLogger());
+
+$entityManager->getConnection()->getDatabasePlatform()->registerDoctrineTypeMapping('enum', 'string');
+
+return ConsoleRunner::createHelperSet($entityManager);
